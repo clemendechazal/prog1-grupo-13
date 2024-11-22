b19769bbeb2de736b2c05d155d834245f0538c21
valencag332 <vcagnolattimantaras@gmail.com>
2024-11-22 11:30:51 -0300

    js de header

--- js/login.js
+++ js/login.js
@@ -1,33 +1,65 @@
+let buscador = document.getElementById("buscador")
+let form = document.querySelector("form")
+
+
+form.addEventListener('click', function (e) {
+    e.preventDefault();
+    let escrito = buscador.value
+
+    if (escrito === "") {
+        alert("Buscador vacío")
+
+    }
+    else if (length(escrito) <= 3) {
+        alert("Se necesitan mas de 3 caracteres")
+    }
+    else if (escrito !== "" && length(escrito) > 3) {
+        form.submit();
+    }
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 let personaEmail = document.getElementById("email_input")
 let personaPassword = document.getElementById("password_input")
 
-let mensajeErrorEmail= document.getElementById("vacioErrorEmail");
-let mensajeErrorPassword= document.getElementById("vacioErrorPass");
+let mensajeErrorEmail = document.getElementById("vacioErrorEmail");
+let mensajeErrorPassword = document.getElementById("vacioErrorPass");
 
 let registerForm = document.querySelector("form")
 
 let button = document.getElementById('button_login');
 
-button.addEventListener('click', function(){
-if (personaEmail.value === ""){
-    mensajeErrorEmail.innerText = "Ingrese email!"
-}
-else {
-    mensajeErrorEmail.innerText = ""
-}
-if (personaPassword.value === ""){
-    mensajeErrorPassword.innerText = "Ingrese contraseña!"
-}
-else {
-    mensajeErrorPassword.innerText = ""
-}
+button.addEventListener('click', function () {
+    if (personaEmail.value === "") {
+        mensajeErrorEmail.innerText = "Ingrese email!"
+    }
+    else {
+        mensajeErrorEmail.innerText = ""
+    }
+    if (personaPassword.value === "") {
+        mensajeErrorPassword.innerText = "Ingrese contraseña!"
+    }
+    else {
+        mensajeErrorPassword.innerText = ""
+    }
 
-if(personaEmail.value !== "" && personaPassword.value !== "") {
-    registerForm.submit()
-}
+    if (personaEmail.value !== "" && personaPassword.value !== "") {
+        registerForm.submit()
+    }
 })
 
 
