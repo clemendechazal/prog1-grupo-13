618b2f5df45e938c1f80013174aa8a1903b6fdd6
clemendechazal <cdechazal@udesa.edu.ar>
2024-11-22 11:29:38 -0300

    agregue la paguina y trabaje search-results

--- js/search-results.js
+++ js/search-results.js
@@ -1,7 +1,8 @@
-fetch(`https://dummyjson.com/recipes`)
+fetch(`https://dummyjson.com/recipes/${final}`)
 .then(function(response) {
   return response.json()
 })
+
 .then(function(data) {
     let search = document.querySelector(".searchResults");
     let results = document.querySelector(".results")
@@ -9,14 +10,14 @@ fetch(`https://dummyjson.com/recipes`)
     let idGet = location.search;
     let params = URLSearchParams(idGet);
     let final = params.get("clemen");
-    results.innerText = `Resultado de la busqueda: ${}`
+    results.innerText = `Resultado de la busqueda: ${final}`
    
     for (let i = 0; i < recetas.length; i++) {
-        let receta = recetas[i];
-        if (receta.name == final){
+        let recetas = recetas[i];
+        if (recetas.name == final){
             contenido += `
             <article class = "article_index">
-                <img src="${recetas[i].image}" alt="${receta[i].name}">
+                <img src="${recetas[i].image}" alt="${recetas[i].name}">
                 <h3>${recetas[i].name}</h3>
                 <p>Nivel de dificultad: ${recetas[i].difficulty}</p>
                 <a class = "a_index" href="./recetas.html?idReceta=${recetas[i].id}">Ver detalle</a>
@@ -25,9 +26,10 @@ fetch(`https://dummyjson.com/recipes`)
     }
 
     if (contenido == ""){
-        contenido = `<h3>No se encontraron resultados a su busqueda</h3>;`
+        contenido = `<h3>No se encontraron resultados a su busqueda</h3>`;
     }
+    search.innerHTML = contenido;
 })
 .catch(function(error) {
   console.log("Error: " + error);
-})
+});
