8735bced97ff079388cae57986db2dbbf7a4aebb
valencag332 <vcagnolattimantaras@gmail.com>
2024-11-22 01:29:23 -0300

    modificaciones de HTML y JS para login y register

--- js/login.js
+++ js/login.js
@@ -1,3 +1,31 @@
-function login(){
-    document.querySelector("a")
+
+
+let presslogin = false
+
+let personaEmail = document.getElementById("email_input")
+let personaPassword = document.getElementById("password_input")
+
+let mensajeErrorEmail= document.getElementById("vacioErrorEmail");
+let mensajeErrorPassword= document.getElementById("vacioErrorPass");
+
+let registerForm = document.querySelector("form")
+
+let button = document.getElementById('button_login');
+button.addEventListener('click', function(){
+if (personaEmail.value === ""){
+    mensajeErrorEmail.innerText = "Ingrese email!"
 }
+else {
+    mensajeErrorEmail.innerText = ""
+}
+if (personaPassword.value === ""){
+    mensajeErrorPassword.innerText = "Ingrese contraseña!"
+}
+else {
+    mensajeErrorPassword.innerText = ""
+}
+
+if(personaEmail.value !== "" && personaPassword.value !== "") {
+    registerForm.submit()
+}
+})
--- js/register.js
+++ js/register.js
@@ -1,19 +1,29 @@
-let nombre = location.search;
-let nombreMensaje = new URLSearchParams(nombre);
 
-nombreMensaje.get('buscador');
+let presslogin = false
 
+let personaEmail = document.getElementById("email_input")
+let personaPassword = document.getElementById("password_input")
 
+let mensajeErrorEmail= document.getElementById("vacioErrorEmail");
+let mensajeErrorPassword= document.getElementById("vacioErrorPass");
 
+let registerForm = document.querySelector("form")
 
-
-
-
-
-
-
-
-
-
-
-
+let button = document.getElementById('button_login');
+button.addEventListener('click', function(){
+if (personaEmail.value === ""){
+    mensajeErrorEmail.innerText = "Ingrese email!"
+}
+else {
+    mensajeErrorEmail.innerText = ""
+}
+if (personaPassword.value === ""){
+    mensajeErrorPassword.innerText = "Ingrese contraseña!"
+}
+else {
+    mensajeErrorPassword.innerText = ""
+}
+if(personaEmail.value !== "" && personaPassword.value !== "") {
+    registerForm.submit()
+}
+})
