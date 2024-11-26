516fbc949cf9406c0dda43454ac64d87703c5cfa
SofiaGMonzo <smonzo@udesa.edu.ar>
2024-11-26 09:57:06 -0300

    modifique el js de search-results

--- js/search-results.js
+++ js/search-results.js
@@ -1,3 +1,41 @@
+
+
+let idGet = location.search;
+let params = new URLSearchParams(idGet);
+let final = params.get("buscador");
+let searchRecipe = document.querySelector(".lista-index")
+let contenido = ""
+
+fetch(`https://dummyjson.com/recipes/?q=${final}`)
+.then(function(response) {
+  return response.json();
+})
+
+.then(function(data) {
+    for (let i = 0; i < recetas.length; i++){
+        let recetas = data.recipes; 
+
+        let contenidos = `
+            <article class = "article_index">
+                <img src="${recetas[i].image}" alt="${recetas[i].name}">
+                <h3>${recetas[i].name}</h3>
+                <p>Nivel de dificultad: ${recetas[i].difficulty}</p>
+                <a class = "a_index" href="./recetas.html?idReceta=${recetas[i].id}">Ver detalle</a>
+            </article>
+            `;
+        
+        contenido += contenidos;
+    }
+
+    searchRecipe.innerHTML = contenido;
+})
+
+.catch(function(error) {
+  console.log("Error: " + error);
+});
+
+
+/** */
 fetch(`https://dummyjson.com/recipes/${final}`)
 .then(function(response) {
   return response.json()
@@ -9,7 +47,7 @@ fetch(`https://dummyjson.com/recipes/${final}`)
     let resultados = ""
     let idGet = location.search;
     let params = URLSearchParams(idGet);
-    let final = params.get("clemen");
+    let final = params.get("query");
     results.innerText = `Resultado de la busqueda: ${final}`
    
     for (let i = 0; i < recetas.length; i++) {
