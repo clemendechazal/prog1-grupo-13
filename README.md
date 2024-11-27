15d60e9edd11f63e79457829f1cd70b29c39c829
clemendechazal <cdechazal@udesa.edu.ar>
2024-11-27 15:42:58 -0300

    arreglos en categoria

--- README.md
+++ README.md
@@ -1,5 +1,1 @@
 # Programacion-trabajo-final
-Falta trabajar:
-clemen: modificar unas cuantas cosas de la 4 paguina y hacer la 6.
-valen:
-sofi:
--- js/category.js
+++ js/category.js
@@ -2,7 +2,9 @@ let qs = location.search;
 let qsObj = new URLSearchParams(qs);
 let idTag = qsObj.get("idTag");
 let lista = document.querySelector(".lista-index");
+let h1Titulo = document.querySelector(".h1-index");
 let url = `https://dummyjson.com/recipes/tag/${idTag}`
+
 
 fetch(url)
     .then(function(response) {
@@ -12,6 +14,10 @@ fetch(url)
         console.log(data.recipes); 
         let contenido = "";
         let recetas = data.recipes; 
+        
+        if (data.tag) { 
+            h1Titulo.innerHTML = `Categoría de ${data.tag}`;
+        }
 
         for (let i = 0; i < recetas.length; i++) {
             contenido += `
@@ -22,7 +28,12 @@ fetch(url)
                     <a class = "a_index" href="./recetas.html?idReceta=${recetas[i].id}">Ver detalle</a>
                 </article>`;
         }
+    
         lista.innerHTML += contenido; 
+
+        if (recetas.length > 0) {
+            h1Titulo.textContent = `Categoría de: ${idTag}`;
+        }
     })
     .catch(function(err) {
         console.error('Fetch error:', err);
